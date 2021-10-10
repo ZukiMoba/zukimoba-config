7d4f3bc05c605d9a2fb97de0f53caf0405c52b94
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-10-10 18:18:51 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -12,7 +12,7 @@
     "content": [
       {
         "is_new": "Wed Oct 08 2021 15:22:23 GMT+0700",
-        "cover": "/images/logos/vero-logo-03.png",
+        "cover": "https://market.verofarm.com/images/logos/vero-logo-03.png",
         "title": "Marketplace Grand Opening",
         "description": "Welcome to Vero Marketplace - Start to buy or sell Vero NFT now",
         "time": "Sun Oct 10 2021 21:00:00 GMT+0700",
