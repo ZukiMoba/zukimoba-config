462eaf9b620e5efc2c631396c738550d5850348b
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-10-10 17:58:06 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -13,27 +13,9 @@
       {
         "is_new": "Wed Oct 08 2021 15:22:23 GMT+0700",
         "cover": "/images/logos/vero-logo-03.png",
-        "title": "UFIN",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisc",
-        "time": "Wed Oct 06 2021 15:22:23 GMT+0700",
-        "link": "",
-        "target": ""
-      },
-      {
-        "is_new": "Wed Oct 06 2021 15:22:23 GMT+0700",
-        "cover": "/images/logos/vero-logo-03.png",
-        "title": "Vero Garden Play",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipisc adipisc adipisc adipisc.",
-        "time": "Wed Oct 06 2021 15:19:23 GMT+0700",
-        "link": "https://garden.verofarm.com",
-        "target": "_blank"
-      },
-      {
-        "is_new": "",
-        "cover": "/images/logos/vero-logo-03.png",
-        "title": "Vero market",
-        "description": "Vero ipsum 1000 points dolor sit amet, consecte",
-        "time": "Wed Oct 05 2021 15:19:23 GMT+0700",
+        "title": "Marketplace Grand Opening",
+        "description": "Welcome to Vero Marketplace - Start to buy or sell Vero NFT now",
+        "time": "Sun Oct 10 2021 21:00:00 GMT+0700",
         "link": "",
         "target": ""
       }
