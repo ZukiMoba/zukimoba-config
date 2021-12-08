ac6682b1235d489b031a83688397b298b2add409
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-12-08 11:47:28 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -13,12 +13,12 @@
       {
         "id": "1",
         "is_new": "Wed Oct 08 2021 15:22:23 GMT+0700",
-        "cover": "/images/logos/logo-03.png",
+        "cover": "/images/logos/logo.png",
         "title": "Marketplace Grand Opening",
         "description": "Welcome to Zuki Marketplace - Start to buy or sell Zuki NFT now",
-        "time": "Fri Dec 03 2021 11:20:24 GMT+0700",
+        "time": "Friday December 3, 2021, 11:20:24 AM",
         "link": "",
-        "link_title": "Open your zuki",
+        "link_title": "",
         "target": ""
       }
     ]
