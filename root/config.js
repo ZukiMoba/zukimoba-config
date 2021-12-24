a35f2ca9af19e326ad3af605ecd708e242bab1e5
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-12-24 17:58:52 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -29,9 +29,9 @@
     }
   },
  "market": {
-    "disableBuy": 0,
-    "disableSale": 0,
-    "disableCancel": 0,
-    "disableSend": 0
+    "disableBuy": 1,
+    "disableSale": 1,
+    "disableCancel": 1,
+    "disableSend": 1
   }
 }
