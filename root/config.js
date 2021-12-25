3cb1aa62ab9a15f781bb7268abd223b07023e278
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-12-25 21:59:48 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -10,12 +10,67 @@
     "auto_open": false,
     "title": "NOTIFICATIONS",
     "content": [
+        {
+        "id": "6"
+        "title": "Update Farming v2 add to news Pool ZUKI-BUSD and ZUKI-USDT",
+        "description": "Welcome to Farming v2 - Start Farming to Earn ZP Now",
+        "cover": "/images/logos/logo.png",
+        "is_new": " Monday December 27, 2021 5:00:00 UTC ",
+        "time": " Monday December 27, 2021 5:00:00 UTC ",
+        "link": "",
+        "link_title": "",
+        "target": ""
+      },
+        {
+        "id": "5"
+        "title": "Maintaining marketplace to upgrade v2",
+        "description": "Please don’t offer to sell any NFTs on marketplace during maintenance ",
+        "cover": "/images/logos/logo.png",
+        "is_new": "Saturday December 25, 2021 16:00:00 UTC to Tuesday January 04,2021 16:00:00 UTC",
+        "time": "Saturday December 25, 2021 16:00:00 UTC to Tuesday January 04,2021 16:00:00 UTC",
+        "link": "",
+        "link_title": "",
+        "target": ""
+      },
+       {
+        "id": "4",
+        "title": "Opening Round 2 of ZP Shop ",
+        "description": "Welcome to ZP Shop - Start to buy and open the Lucky Chest to receive NFT pieces now",
+        "cover": "/images/logos/logo.png",
+        "is_new": "Friday December 24, 2021 16:00:00 UTC ",
+        "time": "Friday December 24, 2021 16:00:00 UTC",
+        "link": "",
+        "link_title": "",
+        "target": ""
+      },
+       {
+        "id": "3",
+        "title": "Update farming v2 add to new Pools ZUKI-BNB",
+        "description": "Welcome to Farming v2 - Start to Farming to Earn ZP Now",
+        "cover": "/images/logos/logo.png",
+        "is_new": "Wednesday December 22, 2021 5:00:00 UTC",
+        "time": "Wednesday December 22, 2021 5:00:00 UTC",
+        "link": "",
+        "link_title": "",
+        "target": ""
+      },
+       {
+        "id": "2",
+        "title": "ZP Shop Grand Opening ",
+        "description": "Welcome to ZP Shop - Start to buy and open the Lucky Chest to receive NFT pieces now",
+        "cover": "/images/logos/logo.png",
+        "is_new": "Sunday December 19,2021 5:00:00 UTC",
+        "time": "Sunday December 19,2021 5:00:00 UTC",
+        "link": "",
+        "link_title": "",
+        "target": ""
+      },
       {
         "id": "1",
-        "is_new": "Wednesday December 15, 2021 5:00:00 UTC",
-        "cover": "/images/logos/logo.png",
         "title": "Farming LPs ZUKI-BNB Grand Opening",
         "description": "Welcome to Zuki Farming - Start Farming to Earn ZP Now",
+        "cover": "/images/logos/logo.png",
+        "is_new": "Wednesday December 15, 2021 5:00:00 UTC",
         "time": "Wednesday December 15, 2021 5:00:00 UTC",
         "link": "",
         "link_title": "",
