18495f563365f6303f5439374c3c1da7f13e2eb5
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-12-30 09:49:33 +0700

    Update config.js

--- root/shopYgg/config.js
+++ root/shopYgg/config.js
@@ -1,7 +1,7 @@
 {
   "zpPriceDollar": 80,
   "round": {
-    "start":             "Dec 30 2021 05:00:00 GMT",
+    "start":             "Dec 30 2020 05:00:00 GMT",
     "end_round_1":       "Jan 05 2022 05:00:00 GMT"
   }
 }
