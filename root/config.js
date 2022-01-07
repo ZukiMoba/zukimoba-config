b81a5575ea7db61c93a0382be84deeac29861745
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2022-01-07 17:14:28 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -10,6 +10,28 @@
     "auto_open": false,
     "title": "NOTIFICATIONS",
     "content": [
+       {
+        "id": 9,
+        "title": "Re-Opening Round 4 of ZP Shop",
+        "description": "Welcome to ZP Shop - Start to buy and open the Lucky Chest to receive NFT pieces now.",
+        "cover": "/images/logos/logo.png",
+        "is_new": "Monday December 27, 2021 5:00:00 UTC ",
+        "time": "from 5:00 UTC January 8,2022 to 5:00 UTC January 15, 2022",
+        "link": "",
+        "link_title": "",
+        "target": ""
+      },
+       {
+        "id": 8,
+        "title": "Marketplace Maintainance",
+        "description": "Marketplace will be temporarily closed for bot-related maintenance, please do not Buy/Sell until we notify you.",
+        "cover": "/images/logos/logo.png",
+        "is_new": "Monday December 27, 2021 5:00:00 UTC ",
+        "time": "from 16:00 UTC January 6,2022 to 5:00 UTC January 10, 2022",
+        "link": "",
+        "link_title": "",
+        "target": ""
+      },
       {
         "id": 7,
         "title": "Re-Opening Round 3 of ZP Shop",
